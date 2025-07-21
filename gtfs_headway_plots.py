d1dd30ed9f450da06a4da78cc5dedaeff9538dad
CherryLiu <133184478+cherryqliu@users.noreply.github.com>
2025-07-21 15:43:29 -0400

    Headway Estimation Update
    Update headway estimation from option 1 to options 2, which is (time_j_n – time_j_1) / (n-1).

--- gtfs_headway_plots.py
+++ gtfs_headway_plots.py
@@ -9,19 +9,19 @@ import pandas as pd
 import numpy as np
 import geopandas as gpd
 from shapely.geometry import LineString
+from datetime import timedelta
+#from gtfs_functions import Feed
 import matplotlib.pyplot as plt
-from gtfs_functions import Feed
 import matplotlib.backends.backend_pdf as pdf_backend
-from shapely.geometry import LineString
 _join = os.path.join
 
-gtfs_location = r'P:\Modeling and Mobility\Data_Requests\TransitDataRequested_bySEMCOG'
-print (gtfs_location)
-sub_path = r'AAATA\GTFSExportJan2023 Elyse Osterday'
-routes_df = pd.read_csv(_join(gtfs_location, sub_path, 'routes.txt'))
-stops_df = pd.read_csv(_join(gtfs_location, sub_path,'stops.txt'))
-stop_times_df = pd.read_csv(_join(gtfs_location, sub_path,'stop_times.txt'))
-trips_df = pd.read_csv(_join(gtfs_location, sub_path,'trips.txt'))
+GTFS_location = r'C:\cliu\Python_Scripts\0_test_folder_keep\GTFS'
+
+sub_path = r'AAATA_GTFS'
+auth = ''
+
+stop_times_df = pd.read_csv(_join(GTFS_location, sub_path,'stop_times.txt'))
+trips_df = pd.read_csv(_join(GTFS_location, sub_path,'trips.txt'))
 
 def headway(service = 2):
     '''
@@ -30,11 +30,26 @@ def headway(service = 2):
     '''
     print ('Calculating headway by time period...')
 
-    merged_df = stop_times_df.merge(trips_df, on='trip_id')
-    merged_df = merged_df.merge(stops_df, on='stop_id')
-    merged_df = merged_df[merged_df['service_id'] == service] 
+    # Calculate route frequency
+    model = 'ABM'
+    if model == 'E8':
+        time_dic = {'AM':['06:30:00', '08:59:59'],'MD':['09:00:00', '14:29:59'],'PM':['14:30:00', '18:29:59'],\
+                    'EV':['18:30:00', '21:59:59'], 'NT1':['22:00:00', '23:59:59'], 'NT2':['0:00:00', '6:29:59'], \
+                    'NT3':['22:00:00', '30:29:59']}  # E8
+    elif model == 'ABM':
+        time_dic = {'AM':['06:30:00', '08:59:59'],'MD':['09:00:00', '14:29:59'],'PM':['14:30:00', '18:29:59'],\
+                    'EV1':['18:30:00', '26:59:59'], 'EV2':['0:00:00', '2:59:59'], 'NT1':['3:00:00', '06:29:59'], 'NT2':['27:00:00', '30:29:59']} # ABM
+    duration = {'AM': timedelta(hours=2, minutes=30),  #ABM
+                'MD': timedelta(hours=5, minutes=30),
+                'PM': timedelta(hours=4),
+                'EV': timedelta(hours=8, minutes=30),
+                'NT': timedelta(hours=3, minutes=30)}
+        
+    merged_df0 = stop_times_df.merge(trips_df, on='trip_id')
+    merged_df = merged_df0[merged_df0['service_id'] == 2] # Temperarily comment for LED 1/3!!! SMART, DDOT UMI 2-weekday!, AAATA 3-weekday.
+    #merged_df = merged_df0 # for LED 2/3!!!
 
-    # Only keep same stop_id with earlist arrival time   
+    # only keep same stop_id with earlist arrival time   
     merged_df['arrival_time'] = pd.to_timedelta(merged_df['arrival_time'])
     earliest_indices = merged_df.groupby('shape_id')['arrival_time'].idxmin()
     earliest_rows = merged_df.loc[earliest_indices]  
@@ -52,16 +67,18 @@ def headway(service = 2):
         # Create a timedelta object
         return pd.to_timedelta(total_seconds, unit='s')
 
+    # print out intermediate_result of time difference - test only!
+    def calculate_intermediate_result(x):
+        intermediate_result = (x['departure_time'].max() - x['departure_time'].min()) / (len(x) - 1) \
+            if len(x) > 1 else duration[time]
+
+        print(f"Intermediate result: {intermediate_result}")
+        return intermediate_result
+
     df = df2 =headway_df = pd.DataFrame()
-    
     # Convert 'departure_time' to timedelta
     merged_df['departure_time'] = merged_df['departure_time'].apply(convert_to_timedelta) # Temperarily comment for LED!!!
 
-    # EV1, NT1 represents the end of day service; while EV2, NT2 represents the beginning of day service
-    time_dic = {'AM':['06:30:00', '08:59:59'],'MD':['09:00:00', '14:29:59'],'PM':['14:30:00', '18:29:59'],\
-                    'EV1':['18:30:00', '26:59:59'], 'EV2':['0:00:00', '2:59:59'], 'NT1':['27:00:00', '30:29:59'], 'NT2':['3:00:00', '06:29:59']}
-    
-    # Assign time of day (TOD) to trips.
     for time in ['AM', 'MD', 'PM', 'EV1', 'EV2', 'NT1', 'NT2']:    
         
         # Define user-defined time period
@@ -76,71 +93,71 @@ def headway(service = 2):
         
         most_common_stops = filtered_df.groupby(['shape_id', 'direction_id'])['stop_id'].agg(lambda x: x.value_counts().idxmax())
         filtered_df = filtered_df[filtered_df['stop_id'].isin(most_common_stops)]
-        filtered_df['TOD'] = time[:2] # [:2] - assign EV1 and EV2 as EV, etc.
-        df = df.append(filtered_df, ignore_index=True)
-    
-    # Calcuate headway
-    for time, min in [['AM', 150], ['MD',300], ['PM', 240], ['EV',510], ['NT', 210]]:
-
+        filtered_df['TOD'] = time[:2]    
+        df = pd.concat([df, filtered_df], ignore_index=True)
+        
+    for time in ['AM', 'MD', 'PM', 'EV', 'NT']:    
+    #for time in ['NT']:    
         filtered_df2 = df[df['TOD'] == time]
         # Subtract one day for values greater than '1 days 03:00:00'
         threshold = pd.to_timedelta('1 days 03:00:00')
         filtered_df2.loc[df['departure_time'] > threshold, 'departure_time'] -= pd.to_timedelta('1 days')
         
-        # Count number of buses    
-        filtered_df2['buses'] = filtered_df2.groupby(['shape_id', 'direction_id', 'stop_id'])['shape_id'].transform('count')
-        
+        # Calculate time difference    
+        filtered_df2['time_diff'] = filtered_df2.groupby(['shape_id', 'direction_id', 'stop_id'])\
+                                                     ['departure_time'].diff().dt.total_seconds()/60
         filtered_df2 = filtered_df2[['arrival_time','departure_time','stop_id','stop_sequence','timepoint',\
-                                   'shape_id','trip_headsign','direction_id','stop_code','stop_name','buses']]   
-            
-        df2 = df2.append(filtered_df2, ignore_index=True)
+                                   'shape_id','route_id','trip_headsign','direction_id','time_diff']]    
+        df2 = pd.concat([df2, filtered_df2], ignore_index=True)
         # Consolidate headways for each route/direction by time period
-        grouped = filtered_df2.groupby(['shape_id', 'trip_headsign', 'direction_id', 'stop_id'])
-        
+        grouped = filtered_df2.groupby(['shape_id', 'route_id', 'trip_headsign', 'direction_id', 'stop_id'])
+
         if not filtered_df2.empty:
-            result_df = grouped.apply(lambda x:  int(min / len(x) + 0.5) if len(x) > 0 else 999).reset_index()
-            result_df.columns = ['shape_id', 'trip_headsign', 'direction_id', 'stop_id', time]
+            result_df = grouped.apply(lambda x: (x['departure_time'].max() - x['departure_time'].min()) / (len(x) - 1) \
+                                   if len(x) > 1 else duration[time]).reset_index()
+
+            #result_df = grouped.apply(calculate_intermediate_result).reset_index(name='intermediate_result') - test only
+            
+            
+            result_df.columns = ['shape_id', 'route_id', 'trip_headsign', 'direction_id', 'stop_id', time]
+            result_df[time] = result_df[time].apply(lambda x: int(x.total_seconds() / 60 + 0.5) if isinstance(x, pd.Timedelta)\
+                                                              else 0)
 
             result_df = result_df.sort_values(by=[time], ascending=[False])
-            result_df = result_df.drop_duplicates(subset=['shape_id', 'trip_headsign', 'direction_id'], keep='first')
+            result_df = result_df.drop_duplicates(subset=['shape_id', 'route_id', 'trip_headsign', 'direction_id'], keep='first')
 
             if time == 'AM':
-                headway_df = headway_df.append(result_df[['shape_id', 'trip_headsign', 'direction_id', time]], ignore_index=True)
+                headway_df = pd.concat([headway_df, result_df[['shape_id', 'route_id', 'trip_headsign', 'direction_id', time]]], ignore_index=True)
             else:
-                headway_df = headway_df.merge(result_df[['shape_id', 'trip_headsign', 'direction_id', time]], \
-                                            on = ['shape_id', 'trip_headsign', 'direction_id'], how = 'outer')
+                headway_df = headway_df.merge(result_df[['shape_id', 'route_id', 'trip_headsign', 'direction_id', time]], \
+                                            on = ['shape_id', 'route_id', 'trip_headsign', 'direction_id'], how = 'outer')
             # replace 0 with 999    
-            headway_df[time]=headway_df[time].replace(0, 999) 
+            #headway_df[time]=headway_df[time].replace(0, 999) 
 
-
-    headway_df.fillna(999, inplace=True) 
-    
-    # Drop records with headway 999 for all time period. Note: for weekends and holidays, some time period, e.g. NT, might not have service.
-    time_columns = ['AM', 'MD', 'PM', 'EV', 'NT']
-    existing_columns = [col for col in time_columns if col in headway_df.columns]
-    if existing_columns:
-        sum_columns = headway_df[existing_columns].sum(axis=1)
-        threshold_value = len(existing_columns) * 999
-        headway_df = headway_df.drop(headway_df[sum_columns == threshold_value].index)
-
-    # Export headway result
-    df2.to_csv( 'time_diff.csv', index=False)
-    headway_df.to_csv('headway.csv', index=False)
+    headway_df.fillna(999, inplace=True)   
+    # Temperarily comment for LED 3/3!!!
+    if 'NT' in headway_df.columns:
+        headway_df = headway_df.drop(headway_df[(headway_df['AM'] + headway_df['MD'] + headway_df['PM'] + headway_df['EV'] + headway_df['NT']) == 4995.0].index)
+    else:
+        headway_df = headway_df.drop(headway_df[(headway_df['AM'] + headway_df['MD'] + headway_df['PM'] + headway_df['EV']) == 4995.0].index)
+        
+    df2.to_csv(auth + '_time_diff.csv', index=False)
+    headway_df.to_csv(auth + '_headway.csv', index=False)
     return headway_df
 
 def plot_routes(headway_df, service = 2):
     print ('Reading in background map...')
-    map_location = r'C:\Users\cliu\Work\Python Practice'
-    street_map = gpd.read_file(_join(map_location, 'Shape File', 'All_Street.shp')) #Read in backgrourd map
     
-    # Plot route itineraries with background street map
-    print ('Plotting transit routes...')
-    shapes_df = pd.read_csv(_join(gtfs_location, sub_path, 'shapes.txt'))
-    shapes_df = trips_df.merge(shapes_df, on='shape_id', how='left')
-    shapes_df = shapes_df[shapes_df['service_id'] == service]
+    map_location = r'C:\cliu\Python_Scripts\0_test_folder_keep\GTFS'
+    street_map = gpd.read_file(_join(map_location, 'All Street Shape File\All_Street.shp')) #SEMCOG ??? map (All Street map is used for ABM)
+    
+    # Plot TRAU itineraries with background street map
+    shapes_df2 = pd.read_csv(_join(GTFS_location, sub_path, 'shapes.txt'))
+    shapes_df = trips_df.merge(shapes_df2, on='shape_id', how='left')
+    shapes_df = shapes_df[shapes_df['service_id'] == 2] # temporarily comment for UMI, #SMART 2-weekday!
     shapes_df = shapes_df.drop_duplicates(subset=['shape_id', 'shape_pt_sequence']) # removal candidate
 
-    shapes_df['route_id'] = shapes_df['route_id'].astype(int) # placeholder, can be plotted.
+    shapes_df['route_id'] = shapes_df['route_id'].astype(int) # need comment for UMI
     time_columns = ['shape_id', 'AM', 'MD', 'PM', 'EV', 'NT']
     existing_columns = [col for col in time_columns if col in headway_df.columns]
 
@@ -151,14 +168,16 @@ def plot_routes(headway_df, service = 2):
     
     # Create a PDF file to store the plots WITHOUT stops
     unique_routes = np.sort(shapes_df['shape_id'].unique())
-    pdf_pages = pdf_backend.PdfPages(_join(map_location, 'output', 'plots_with_allstreet.pdf'))
+    unique_routes = sorted(unique_routes, key=lambda x: tuple(map(int, x.replace('shp-', '').split('-')))) # need comment for UMI
+    pdf_pages = pdf_backend.PdfPages(_join(map_location, 'All Street Shape File', 'plots_with_geojson.pdf'))
 
     # Iterate over routes and plot their itineraries
     for route in unique_routes:
         # Filter route data
         route_data_df = shapes_df[shapes_df['shape_id'] == route]
         shape_id = route_data_df['shape_id'].iloc[0]
-        route_name = route_data_df['trip_headsign'].iloc[0]
+        route_id = route_data_df['route_id'].iloc[1]
+        route_name = shape_id + " " + route_data_df['trip_headsign'].iloc[0]
         route_freq = route_data_df['freq'].iloc[0].split(',')
         modified_values = [value.replace('.0', '') for value in route_freq]
         route_freq = ','.join(modified_values)       
@@ -181,29 +200,35 @@ def plot_routes(headway_df, service = 2):
             if i == 0:
                 label = 'A'
                 color = 'red'
+                #print ('a', route, route_data_df['shape_pt_lon'], route_data_df['shape_pt_lat'])
                 ax.text(route_data_df['shape_pt_lon'].iloc[i], route_data_df['shape_pt_lat'].iloc[i], label, fontsize=12, color=color, fontweight='bold')
             elif i == len(route_data_df['shape_pt_lon']) - 1:
                 label = 'B'
                 color = 'green'
+                #print ('a', route, route_coords[i][0], route_coords[i][1])
                 ax.text(route_data_df['shape_pt_lon'].iloc[i], route_data_df['shape_pt_lat'].iloc[i], label, fontsize=12, color=color, fontweight='bold')
             else:
                 continue
 
-        # Plot the cropped street map on top of the route line
+        # Plot the cropped street map on top of the route line with a thinner linewidth
         cropped_map.plot(ax=ax, color='lightgrey', linewidth=0.3)    
         
-        ax.set_title(f'Route Itinerary: {shape_id, route_name, route_freq}')
+        ax.set_title(f'Route Itinerary: {route_id, route_name, route_freq}')
         ax.set_xlabel('Longitude')
         ax.set_ylabel('Latitude')
         ax.grid(False)
         
         # Save the plot as a PDF page
         pdf_pages.savefig()
+        
+        # Show and close the plot
         plt.show()
         plt.close()
-    pdf_pages.close()                                                         
+
+    # Close the PDF file
+    pdf_pages.close()                                  
                                                          
 if __name__ == '__main__':
-    service = 1 # transit agency defined service date. Common practice: 1-Sunday, 2-weeday, 3-Saturday
+    service = 3 # transit agency defined service date. Common practice: 1, 2, 3 - Saturday Sunday, weekday. Values may vary, check calendar.txt for details.
     headway_df = headway(service) 
     plot_routes(headway_df, service)
